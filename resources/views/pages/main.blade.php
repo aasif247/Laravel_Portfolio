c79fa1d205289972febfb3caea6c60ebc062009d
aasif247 <47910560+aasif247@users.noreply.github.com>
2022-01-19 00:12:43 +0600

    create model and migration - create new design in main.blade.php - insert some value in database - image and pdf link are insert in database - link a storage to public via command ( php artisan storage:link )

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -11,6 +11,29 @@
                     <li class="breadcrumb-item active">Main</li>
                 </ol>
 
+                <div class="row">
+                    <div class="form-group col-md-3 mt-3">
+                        <h4>Background Image</h4>
+                        <img style="height:30vh" src="{{ asset('assets/img/header-bg.jpg') }}" class="img-thumbnail" >
+                        <input class="mt-3" type="file" id="bg_img" name="bg_img">
+                    </div>
+
+                    <div class="form-group col-md-4 mt-3">
+                        <div class="mb-3">
+                            <label for="title"><h4>Title</h4></label>
+                            <input type="text" class="form-control" id="title" name="title">
+                        </div>
+
+                        <div class="mb-4">
+                            <label for="sub_title"><h4>Sub Title</h4></label>
+                            <input type="text" class="form-control" id="sub_title" name="sub_title">
+                        </div>
+                        <div>
+                            <h4>Upload Resume</h4>
+                            <input class="mt-2" type="file" id="resume" name="resume">
+                        </div>
+                    </div>
+                </div>
             </div>
         </main>
 @endsection              
