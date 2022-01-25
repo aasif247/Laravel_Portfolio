b3a9f55687bd554a28092338e62347e675ca99de
aasif247 <47910560+aasif247@users.noreply.github.com>
2022-01-25 12:09:36 +0600

    fixed some code and image size in portfolio section

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -74,17 +74,17 @@
                     <h2 class="section-heading text-uppercase">Portfolio</h2>
                     <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
                 </div>
-                <!-- Portfolio item 1-->
+               
                 <div class="row">            
                     @if (count($portfolios)>0)
                        @foreach ($portfolios as $portfolio)
                        <div class="col-lg-4 col-sm-6 mb-4">
                         <div class="portfolio-item">
-                            <a class="portfolio-link" data-bs-toggle="modal" href="#portfolioModal1<?php echo $portfolio->id?>">
+                            <a class="portfolio-link" data-bs-toggle="modal" href="#portfolioModal<?php echo $portfolio->id?>">
                                 <div class="portfolio-hover">
                                     <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                                 </div>
-                                <img class="img-fluid" src="{{url($portfolio->small_image)}}" alt="..." />
+                                <img  class="sameImgClass" src="{{url($portfolio->small_image)}}" alt="Portfolio Image" />
                             </a>
                             <div class="portfolio-caption">
                                 <div class="portfolio-caption-heading">{{$portfolio->client}}</div>
@@ -266,7 +266,7 @@
         <!-- Portfolio item 1 modal popup-->
         @if (count($portfolios)>0)
         @foreach ($portfolios as $portfolio)
-        <div class="portfolio-modal modal fade" id="portfolioModal1<?php echo $portfolio->id?>" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="portfolio-modal modal fade" id="portfolioModal<?php echo $portfolio->id?>" tabindex="-1" role="dialog" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal" /></div>
