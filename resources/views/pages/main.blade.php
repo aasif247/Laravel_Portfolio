eb0922966e419965599322385ea425d81a07e75a
aasif247 <47910560+aasif247@users.noreply.github.com>
2022-01-28 11:21:21 +0600

    fixed main information

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -18,26 +18,26 @@
                 <div class="row">
                     <div class="form-group col-md-3 mt-3">
                         <h4>Background Image</h4>
-                        <img style="height:30vh" src="{{ url($main->bg_img)}}" class="img-thumbnail" >
+                        <img style="height:30vh" src="{{(@main->bg_img)?url($main->bg_img):asset("assets/img/bg_img.jpg")}}" class="img-thumbnail" >
                         <input class="mt-3" type="file" id="bg_img" name="bg_img">
                     </div>
 
                     <div class="form-group col-md-3 mt-3">
                         <h4>Logo</h4>
-                        <img style="height:30vh" src="{{ url($main->logo) }}" class="img-thumbnail" >
+                        <img style="height:30vh" src="{{(@main->logo)?url($main->logo):asset("assets/img/logo.png")}}" class="img-thumbnail" >
                         <input class="mt-3" type="file" id="logo" name="logo">
                     </div>
 
                     <div class="form-group col-md-4 mt-3">
                         <div class="mb-3">
                             <label for="title"><h4>Title</h4></label>
-                            <input type="text"  id="title" name="title" value="{{ $main->title }}"
+                            <input type="text"  id="title" name="title" value="{{(@main->title)?$main->title:"The Title"}}"
                             class="form-control">
                         </div>
 
                         <div class="mb-4">
                             <label for="sub_title"><h4>Sub Title</h4></label>
-                            <input type="text" id="sub_title" name="sub_title" value="{{ $main->sub_title }}"
+                            <input type="text" id="sub_title" name="sub_title" value="{{ (@main->sub_title)?$main->sub_title:"The Sub Title" }}"
                             class="form-control">
     
                         </div>
